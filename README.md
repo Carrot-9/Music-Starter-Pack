6e149ca206bfeda93df7d612648e9bc6cb7fd501
Krod-9 <KerrodMcNeal2@gmail.com>
2024-08-26 14:23:47 -0400

    Create Website Project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Music Starter Pack
+ 
+1.What is Music-Starter-Pack? 
+
+A: Music Starter Pack is the name of my first website. This website was made as a way for me to learn the ins and outs of various languages such as HTML, CSS and Javascript.
+
+2.What is the purpose of this website?
+
+A: The purpose of this website is to provide users with album recs curated by none other than me, Carrot! These album recs are seperated and filtered by genre, making it easy to find an album that's just right for you! 
+
+3. Additional Features
+
+My website has an additional feature that allows you to randomly pick one album out of the entire site's database to listen to.
+
+NOTE: As of 8/26/24 this site is unfinished.
---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+ //Initial Question
+ function Genre() {
+   const GenreQuestion = prompt("What genre of music do you want to choose from? Type 'All' to randomize without genre filters.");
+   if (GenreQuestion.toLowerCase() == "rock") {
+   var userChoice = 'Data/RockAlbums.json';
+   } else if (GenreQuestion.toLowerCase() == "hip hop" || GenreQuestion.toLowerCase() == "hip-hop" || GenreQuestion.toLowerCase() == "rap") {
+   var userChoice = 'Data/HipHopAlbums.json';
+   } else if (GenreQuestion.toLowerCase() == "pop") {
+   var userChoice = 'Data/PopAlbums.json';
+   } else {
+      alert("That is not a valid answer. Please try again.")
+      return Genre();
+   }
+
+   AlbumRandomizer();
+
+      // Randomizers
+   async function AlbumRandomizer() {
+   try {
+   const response = await fetch(userChoice);
+   const Data = await response.json();
+   const Albums = Data.Albums;
+   const RANDOM = Albums[Math.floor(Math.random() * Albums.length)];
+   alert(RANDOM.name);
+   document.getElementById('album-image').src = RANDOM.image;
+   document.getElementById('album-title').innerText = RANDOM.name;
+   } catch(error) {
+   console.log("An error has occured:", error)
+      }
+   }
+ }
+
+   
+   
+   
